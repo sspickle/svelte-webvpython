7375004ef24545a4483f59ca5109c4691182696b
Steve Spicklemire <steve@spvi.com>
2022-07-09 13:48:24 -0600

    update

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -6,10 +6,16 @@ const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
 	preprocess: preprocess(),
+	
 
 	kit: {
-		adapter: adapter()
-	}
+        adapter: adapter({
+            pages: "docs",
+            assets: "docs"
+    	})},
+    paths: {
+   	    base: "/svelte-webvpython"
+    }
 };
 
 export default config;
